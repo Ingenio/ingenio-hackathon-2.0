712b47a8193801d296046dac74cda16ded104f2c
mramasubramanian <mramasubramanian@ingenio.com>
2015-10-29 21:13:35 -0700

    Adjusted advisor-info component to be compatible to both for the home page as well as the name your price page.

--- client/app/components/advisor-info.js
+++ client/app/components/advisor-info.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  hasUserOffer: function(){
+    return (this.get('offerPrice') && this.get('offerPrice') > 0);
+  }.property(),
+
   starRatingImageUrl: function() {
     return ('http://i.keen.com/D1_k3ratingstars-' + this.get('advisorInfo').starRating + '_V1.png');
   }.property()
